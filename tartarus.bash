160ab964af4bc5769dd2b360f6e575caf02c515f
root <root@srv001.gosix.net>
2016-01-29 12:34:11 +0100

    embedded logging and making the script a little more error proof

--- tartarus.bash
+++ tartarus.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
 scriptdir="$(dirname "$0")"
 source $scriptdir/tartarus.inc
+logger -t $LOGTAG "script begins"
+logger -t $LOGTAG "Executing in $scriptdir"
 for i in $( ls $basepath/*.conf); do
+	logger -t $LOGTAG "Running profile $i"
 	profile=$(echo $i|awk -F'.' '{print $1}')
-	/usr/sbin/charon.ftp --host $ftphost --user $ftpuser --password $ftppass --profile $profile --maxage $ageindays
-	tartarus $i
+	/usr/sbin/charon.ftp --host $ftphost --user $ftpuser --password $ftppass --profile $profile --maxage $ageindays 2>&1 | logger -t $LOGTAG
+	/usr/sbin/tartarus $i 2>&1 | logger -t $LOGTAG
 done
+logger -t $LOGTAG "script end"
