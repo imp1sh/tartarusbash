1cafe39041af8b2314e53f1184c573e4a3f4fa2a
root <root@srv001.gosix.net>
2016-01-29 12:14:07 +0100

    made sure that the script runs, no matter from which directory you invoke it from. Just make sure the script is in the same path as the tartarus.inc file

--- tartarus.bash
+++ tartarus.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
-# put that script in /etc/tartarus and just run it manually or via cron for example
-source tartarus.inc
+scriptdir="$(dirname "$0")"
+source $scriptdir/tartarus.inc
 for i in $( ls $basepath/*.conf); do
 	profile=$(echo $i|awk -F'.' '{print $1}')
 	/usr/sbin/charon.ftp --host $ftphost --user $ftpuser --password $ftppass --profile $profile --maxage $ageindays
