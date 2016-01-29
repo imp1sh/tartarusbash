fabbeb6c6cfd62b0fc1cc069e59ec88e54e4f1e1
root <root@srv001.gosix.net>
2016-01-29 14:24:47 +0100

    made the script better

--- tartarus.bash
+++ tartarus.bash
@@ -1,17 +1,21 @@
 #!/bin/bash
+echo "Script started, please wait"
+# getting the directory the script is in
 scriptdir="$(dirname "$0")"
+# sourcing the variables from tartarus.inc
 source $scriptdir/tartarus.inc
-logger -t $LOGTAG "script begins"
-logger -t $LOGTAG "Executing in $scriptdir"
-logger -t $LOGTAG "Running script that should run before the backup job"
-./scriptdir/runbefore.bash
+logger -t $LOGTAG "START"
+logger -t $LOGTAG "Running script prior to backup" 
 $scriptdir/runbefore.bash 2>&1 | logger -t $LOGTAG
-for i in $( ls $basepath/*.conf); do
-	logger -t $LOGTAG "Running profile $i"
+# loop that runs through all conf files
+cd $basepath
+for i in $( ls *.conf); do
 	profile=$(echo $i|awk -F'.' '{print $1}')
+	logger -t $LOGTAG "Running profile $profile with corresponding config file $i"
 	/usr/sbin/charon.ftp --host $ftphost --user $ftpuser --password $ftppass --profile $profile --maxage $ageindays 2>&1 | logger -t $LOGTAG
 	/usr/sbin/tartarus $i 2>&1 | logger -t $LOGTAG
 done
-logger -t $LOGTAG "Running script that should run after the backup job"
-./scriptdir/runafter.bash 2>&1 | logger -t $LOGTAG
-logger -t $LOGTAG "script end"
+logger -t $LOGTAG "Running script after backup"
+# running jobs after backup finished
+$scriptdir/runafter.bash 2>&1 | logger -t $LOGTAG
+logger -t $LOGTAG "END"
